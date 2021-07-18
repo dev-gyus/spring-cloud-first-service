0701400ab5dc74a421ce0b5f5620a93fd9d4a3bb
GyuHyeong <cjsworbehd13@gmail.com>
2021-07-19 00:26:50 +0900

    랜덤포트 동작 및 eureka 서비스등록 네이밍 설정

--- src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -2,16 +2,21 @@ package com.example.firstservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    private final Environment environment;
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First service";
@@ -24,7 +29,9 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First-Service";
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First-Service on Port %s", environment.getProperty("local.server.port"));
     }
 }
